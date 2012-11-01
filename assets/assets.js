b7c53a8d5111775ea4abd831251ca9454a92bece
Tapio Vierros <tapiovierros@gmail.com>
2012-11-01 21:23:12 +0200

    Two more barrel models.

--- assets/assets.js
+++ assets/assets.js
@@ -1,6 +1,8 @@
 var assets = {
 	objects: {
 		"ammo-box": { collision: "box", mass: 250 },
+		"barrel-blue": { collision: "cylinder", mass: 250 },
+		"barrel-red": { collision: "cylinder", mass: 250 },
 		"barrel-rusty": { collision: "cylinder", mass: 250 },
 		"gas-tank": { collision: "cylinder", mass: 350 }
 	},
