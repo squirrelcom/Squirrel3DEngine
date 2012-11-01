5fcf15eb45e194beb39506e4547cd0ab964b4650
Tapio Vierros <tapiovierros@gmail.com>
2012-11-01 20:27:34 +0200

    Added assets.js and a test level.

---
+++ assets/assets.js
@@ -0,0 +1,16 @@
+var assets = {
+	objects: {
+		"ammo-box": { collision: "box", mass: 250 },
+		"barrel-rusty": { collision: "cylinder", mass: 250 }
+	},
+	items: {},
+	materials: {},
+	monsters: {
+		"robot": {
+			collision: "box",
+			character: { speed: 40 },
+			animation: { type: "morph", duration: "1000" }
+		}
+	},
+	environments: {}
+};
