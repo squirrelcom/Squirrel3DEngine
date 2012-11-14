965c8361a6d2b320d5e5fe075d1326892830d964
Tapio Vierros <tapiovierros@gmail.com>
2012-11-14 20:22:55 +0200

    Revert "Try to work around Firefox requiring MIME type for audio."
    This reverts commit 775070f0ef27b231e372f758acf73504a5be00f6.
    
    Didn't work :(

--- js/sound.js
+++ js/sound.js
@@ -23,28 +23,15 @@ function Sound(samples, minPlayers) {
 	this.sampleIndex = 0;
 	this.samples = [];
 
-	var useAudioElems = /Firefox/i.test(navigator.userAgent);
-	// Load samples
-	while (this.samples.length < minPlayers) {
-		for (var i = 0; i < samples.length; ++i) {
-			if (useAudioElems) {
-				// Firefox requires server to send the audio with the correct MIME type
-				// so we attempt to work around that by setting it with <audio> tag
-				var elem = document.createElement("audio");
-				var source = document.createElement("source");
-				source.setAttribute("type", "audio/wave");
-				source.setAttribute("src", "assets/sounds/" + samples[i]);
-				elem.appendChild(source);
-				this.samples.push(elem);
-			} else this.samples.push(new Audio("assets/sounds/" + samples[i]));
-		}
-	}
+	while (this.samples.length < minPlayers)
+		for (var i = 0; i < samples.length; ++i)
+			this.samples.push(new Audio("assets/sounds/" + samples[i]));
 
 	this.play = function(volume) {
 		if (!CONFIG.sounds) return;
 		try { // Firefox fails at GitHub MIME types
 			var sample = this.samples[this.sampleIndex];
-			if (window.chrome || useAudioElems) sample.load(); // Chrome requires reload
+			if (window.chrome) sample.load(); // Chrome requires reload
 			else sample.currentTime = 0;
 			if (volume !== undefined)
 				sample.volume = volume;
